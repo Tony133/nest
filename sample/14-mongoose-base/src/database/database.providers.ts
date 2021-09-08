6d08ce67a9d0f2053346becec9de6a9c647feb57
Tony133 <a.tripodi133@gmail.com>
2021-09-08 22:29:05 +0200

    sample(14-mongoose): update database providers for mongodb

--- sample/14-mongoose-base/src/database/database.providers.ts
+++ sample/14-mongoose-base/src/database/database.providers.ts
@@ -3,7 +3,10 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://localhost/nest'),
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect('mongodb://localhost/test', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }),
   },
 ];
